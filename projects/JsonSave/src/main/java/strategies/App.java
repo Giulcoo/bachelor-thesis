e9369b6904ffedb3fd839bd38828d7bbed97179d
Giulio Pazzi <giulio.pazzi02@gmail.com>
2023-09-27 14:16:27 +0200

    feat: Better benchmarks
    Check efficiency of every parameters for the save system in one run

--- projects/JsonSave/src/main/java/strategies/App.java
+++ projects/JsonSave/src/main/java/strategies/App.java
@@ -4,41 +4,6 @@ import strategies.service.GameService;
 
 public class App {
     public static void main(String[] args) {
-//        FileManager.deleteData();
-//        FileManager.createFolders();
-//        FileManager.createFile(Constants.CHANGE_FILE);
-//
-//        ChangeFileService service = new ChangeFileService();
-//
-//        Chunk chunk1 = new Chunk()
-//                .setId("1")
-//                .setPosition(new Vector(1,2))
-//                .setSize(new Vector(3,4));
-//
-//        Chunk chunk2 = new Chunk()
-//                .setId("2")
-//                .setPosition(new Vector(1,2))
-//                .setSize(new Vector(3,4));
-//
-//        Chunk chunk3 = new Chunk()
-//                .setId("3")
-//                .setPosition(new Vector(1,2))
-//                .setSize(new Vector(3,4));
-//
-//        service.saveChunkAdded(chunk1);
-//        service.saveChunkAdded(chunk2);
-//        service.saveChunkAdded(chunk3);
-//
-//        FileManager.readChanges().forEach(n -> {
-//            try {
-//                Chunk c = new ObjectMapper().treeToValue(n.get("value"), Chunk.class);
-//                System.out.println(new ObjectMapper().writerWithDefaultPrettyPrinter().writeValueAsString(c));
-//            }
-//            catch (JsonProcessingException e){
-//                System.out.println("Could not get chunk");
-//            }
-//        });
-
         createGame(100);
         removePlayers(10);
     }
--- projects/JsonSave/src/main/java/strategies/Constants.java
+++ projects/JsonSave/src/main/java/strategies/Constants.java
@@ -13,8 +13,8 @@ public class Constants {
     public static final String DATA_PATH = "data/";
     public static final Path CHUNK_FOLDER = Path.of(DATA_PATH + "chunks");
     public static final String CHUNK_PATH = DATA_PATH + "chunks/";
-    public static final String CHANGE_FILE = DATA_PATH + "changes";
-    public static final String INFO_FILE = DATA_PATH + "info";
+    public static final String CHANGE_FILE = DATA_PATH + "changes.json";
+    public static final String INFO_FILE = DATA_PATH + "info.json";
 
     //Map Settings
     public static final float MAP_SIZE = 1000;
@@ -22,7 +22,7 @@ public class Constants {
     public static final float BOT_SPAWN_DISTANCE_FROM_PLAYER = MAP_SIZE/10;
 
     //Chunk Settings
-    public static final boolean DYNAMIC_CHUNK_SIZE = false;
+    public static boolean DYNAMIC_CHUNK_SIZE = false;
     public static boolean USE_CHANGE_FILE = false;
     public static int STATIC_CHUNK_AMOUNT = 2; //Real amount will be STATIC_CHUNK_AMOUNT * STATIC_CHUNK_AMOUNT
     public static float STATIC_CHUNK_SIZE = MAP_SIZE/STATIC_CHUNK_AMOUNT;
