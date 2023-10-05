8d30a3345f02971b5c689e9c09c62441a97e89ad
Giulcoo <74862175+Giulcoo@users.noreply.github.com>
2023-10-05 07:58:03 +0200

    feat: last missing benchmark values

--- projects/JsonSave/src/jmh/java/benchmarks/DynamicBenchmark.java
+++ projects/JsonSave/src/jmh/java/benchmarks/DynamicBenchmark.java
@@ -7,8 +7,8 @@ import strategies.Constants;
 import java.util.concurrent.TimeUnit;
 
 @Fork(1)
-@Warmup(iterations = 3)
-@Measurement(iterations = 10)
+@Warmup(iterations = 1)
+@Measurement(iterations = 1)
 @OutputTimeUnit(TimeUnit.SECONDS)
 @State(Scope.Benchmark)
 public class DynamicBenchmark {
@@ -18,11 +18,13 @@ public class DynamicBenchmark {
     @Param({"true", "false"})
     public static boolean useGzip;
     //@Param({"1000", "10000", "100000"})
-    @Param({"10000", "100000"})
+    @Param({"1000", "10000"})
     public static int dataCount;
-    @Param({"1000", "5000", "10000"})
+//    @Param({"1000", "5000", "10000"})
+    @Param({"1000"})
     public static int chunkMaxElements;
-    @Param({"2", "10", "100"})
+//    @Param({"2", "10", "100"})
+    @Param({"2"})
     public static int chunkMinElements ;
 
     @State(Scope.Benchmark)
@@ -64,27 +66,27 @@ public class DynamicBenchmark {
 //        App.createGame(dataCount);
 //    }
 
-    @Benchmark
-    public static void loadGame(CreateData createData){
-        Constants.DYNAMIC_CHUNK_SIZE = true;
-        Constants.USE_CHANGE_FILE = useChangeFile;
-        Constants.USE_GZIP = useGzip;
-        Constants.CHUNK_MAX_ELEMENTS = chunkMaxElements;
-        Constants.CHUNK_GROUP_MIN_ELEMENTS = chunkMaxElements/chunkMinElements;
-
-        App.loadGame();
-    }
-
 //    @Benchmark
-//    public static void createPlayers(CreateSmallData createData){
+//    public static void loadGame(CreateData createData){
 //        Constants.DYNAMIC_CHUNK_SIZE = true;
 //        Constants.USE_CHANGE_FILE = useChangeFile;
 //        Constants.USE_GZIP = useGzip;
 //        Constants.CHUNK_MAX_ELEMENTS = chunkMaxElements;
 //        Constants.CHUNK_GROUP_MIN_ELEMENTS = chunkMaxElements/chunkMinElements;
 //
-//        App.createPlayers(dataCount);
+//        App.loadGame();
 //    }
+
+    @Benchmark
+    public static void createPlayers(CreateSmallData createData){
+        Constants.DYNAMIC_CHUNK_SIZE = true;
+        Constants.USE_CHANGE_FILE = useChangeFile;
+        Constants.USE_GZIP = useGzip;
+        Constants.CHUNK_MAX_ELEMENTS = chunkMaxElements;
+        Constants.CHUNK_GROUP_MIN_ELEMENTS = chunkMaxElements/chunkMinElements;
+
+        App.createPlayers(dataCount);
+    }
 //
 //    @Benchmark
 //    public static void removePlayers(CreateData createData){
